307aeb65393275cd0fc12b8b11c0e1e03d21b9c2
miluu <hyxiong@qq.com>
2016-11-29 23:12:31 +0800

    init project

---
+++ src/Main.ts
@@ -0,0 +1,13 @@
+
+class Main extends egret.DisplayObjectContainer {
+
+    public constructor() {
+        super();
+        this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
+    }
+
+    private onAddToStage() {
+    }
+}
+
+
