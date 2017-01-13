3c6554f6b4cf1434fc435bb18f454649369556b3
miluu <huangyx@qq.com>
2017-01-13 17:04:01 +0800

    add jsDoc

--- src/modules/utils/index.ts
+++ src/modules/utils/index.ts
@@ -1,4 +1,11 @@
 module util {
+  /**
+   * 返回距离中心点一定距离和角度的点
+   * @param center {egret.Point} 中心点对象
+   * @param radius {number} 半径
+   * @param degree {number} 角度
+   * @return {egret.Point} 返回距离中心点一定距离和角度的点
+   */
   export function cyclePoint(center: egret.Point, radius: number, degree: number): egret.Point {
     let x = Math.cos(degree * Math.PI / 180) * radius;
     let y = Math.sin(degree * Math.PI / 180) * radius;
