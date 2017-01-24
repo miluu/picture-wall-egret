8c08b60e117b5f902d5cd5890f02bd146316f71d
miluu <huangyx@qq.com>
2017-01-24 15:39:13 +0800

    jsDoc

--- src/modules/swiper/Slide.ts
+++ src/modules/swiper/Slide.ts
@@ -1,47 +1,88 @@
 namespace swiper {
 
+  /**
+   * @class Slide
+   * @author HuangYaxiong <hyxiong@qq.com>
+   */
   export class Slide extends egret.DisplayObjectContainer {
+    /**
+     * @private
+     * @static
+     * slide 实例数量
+     */
     private static _slideCount: number;
 
+    /**
+     * @private 是否是克隆出来的 slide
+     */
     private _isClone: boolean;
+    /**
+     * @private 背景 Shape 对象
+     */
     private _bg: egret.Shape;
+    /**
+     * @private 图片 Bitmap 对象
+     */
     private _image: egret.Bitmap;
+    /**
+     * @private 图片纹理 Texture 对象
+     */
     private _texture: egret.Texture;
+    /**
+     * @private slide 宽度
+     */
     private _slideWidth: number;
+    /**
+     * @private Slide 高度
+     */
     private _slideHeight: number;
+    /**
+     * @private key 标识
+     */
     private _slideKey: number;
-    private _key: number;
-    private _keyText: egret.TextField;
-    private _debugKey: number;
+    /**
+     * @public z 值
+     */
     public z: number;
 
-    constructor(width: number, height: number, texture: egret.Texture, debugKey?: number) {
+    /**
+     * @constructor
+     * @param width {number} slide 宽度
+     * @param height {number} slide 高度
+     * @param texture {egret.Texture} 图片纹理 Texture 对象
+     */
+    constructor(width: number, height: number, texture: egret.Texture) {
       super();
       this._slideWidth = width;
       this._slideHeight = height;
       this._texture = texture;
-      this._debugKey = debugKey;
       this.touchEnabled = true;
       this._init();
-      if (_.isNumber(this._debugKey)) {
-        this._addIndex(debugKey);
-      }
       Slide._slideCount++;
     }
 
-    public get isClone() {
+    /**
+     * @public
+     * @get 是否是克隆出来的
+     * @return {boolean}
+     */
+    public get isClone(): boolean {
       return !!this._isClone;
     }
 
+    /**
+     * @public 生成一个克隆对象
+     * @return {Slide}
+     */
     public clone(): Slide {
-      const clone = new Slide(this._slideWidth, this._slideHeight, this._texture, this._debugKey);
+      const clone = new Slide(this._slideWidth, this._slideHeight, this._texture);
       clone._isClone = true;
-      if (clone._keyText) {
-        clone._keyText.textColor = 0xffff00;
-      }
       return clone;
     }
 
+    /**
+     * @private 初始化
+     */
     private _init() {
       this._setSlideKey();
       if (!this._texture) {
@@ -50,6 +91,9 @@ namespace swiper {
       this._addImage();
     }
 
+    /**
+     * @private 添加背景
+     */
     private _addBg() {
       this._bg = new egret.Shape();
       const g = this._bg.graphics;
@@ -62,6 +106,9 @@ namespace swiper {
       this.addChild(this._bg);
     }
 
+    /**
+     * @private 添加图片
+     */
     private _addImage() {
       if (this._texture) {
         const {textureWidth, textureHeight} = this._texture;
@@ -79,22 +126,11 @@ namespace swiper {
       }
     }
 
+    /**
+     * @private 设置 key 标识
+     */
     private _setSlideKey() {
       this._slideKey = Slide._slideCount;
-    }
-
-    private _addIndex(debugKey: number) {
-      this._key = debugKey;
-      this._keyText = new egret.TextField();
-      this._keyText.text = debugKey.toString();
-      this._keyText.width = this._slideWidth;
-      this._keyText.height = this._slideHeight;
-      this._keyText.x = -this._slideWidth / 2;
-      this._keyText.y = -this._slideHeight / 2;
-      this._keyText.textColor = 0xff0000;
-      this._keyText.textAlign = egret.HorizontalAlign.CENTER;
-      this._keyText.verticalAlign = egret.VerticalAlign.MIDDLE;
-      this.addChild(this._keyText);
     }
   }
 }
--- src/modules/swiper/Swiper.ts
+++ src/modules/swiper/Swiper.ts
@@ -1,3 +1,8 @@
+/**
+ * @namespace Swiper
+ * @author HuangYaxiong <hyxiong@qq.com>
+ */
+
 namespace swiper {
   interface ISwiperOptions {
     viewWidth: number;
@@ -10,33 +15,104 @@ namespace swiper {
     onChange?: (currentOffset?: number) => any;
     selfUpdate?: boolean;
   }
+
+  /**
+   * @const swiper 切换效果
+   */
   export const SWIPER_EFFECT = {
     SLIDE: 'SLIDE',
     CARROUSEL: 'CARROUSEL'
   };
+
+  /**
+   * @class Swiper
+   */
   export class Swiper extends egret.DisplayObjectContainer {
+    /**
+     * @private 所有的 slide 对象
+     */
     private _slides: Slide[] = [];
+    /**
+     * @private 所有的 slide 对象拷贝
+     */
     private _slidesCopy: Slide[] = [];
+    /**
+     * @private slide 的容器对象
+     */
     private _slidesContainer: egret.DisplayObjectContainer;
+    /**
+     * @private 偏移量，一个 slide 位置为 1
+     */
     private _offset: number;
+    /**
+     * @private 视图宽度
+     */
     private _viewWidth: number;
+    /**
+     * @private 视图高度
+     */
     private _viewHeight: number;
+    /**
+     * @private slide 宽度
+     */
     private _slideWidth: number;
+    /**
+     * @private slide 高度
+     */
     private _slideHeight: number;
+    /**
+     * @private CARROUSEL 跑马灯效果的半径大小
+     */
     private _radius: number;
+    /**
+     * @private 背景 Shape 对象
+     */
     private _bg: egret.Shape;
-    private _toutchStart: boolean;
+    /**
+     * @private 开始触摸时的 x 坐标
+     */
     private _startX: number;
+    /**
+     * @private 开始触摸时的时间
+     */
     private _startTime: Date;
+    /**
+     * @private 开始触摸时的偏移量
+     */
     private _startOffset: number;
+    /**
+     * @private 开始触摸标识
+     */
     private _touchStart: boolean;
+    /**
+     * @private 关联的 Tween 对象，用于设置 slide 的回弹效果
+     */
     private _tween: TWEEN.Tween;
+    /**
+     * @private 回弹时间间隔
+     */
     private _backTime: number;
+    /**
+     * @private 滑动效果
+     */
     private _effect: string;
+    /**
+     * @private 是否自动更新，如为 true，则会在 swiper 内监听 ENTER_FRAME 并执行 TWEEN.update()
+     */
     private _selfUpdate: boolean;
+    /**
+     * @public 额外关联的 Tween 对象
+     */
     public tween: TWEEN.Tween;
+    /**
+     * @public 切换 slide 完成时的回调事件
+     */
     public onChange: (currentIndex?: number) => any;
 
+    /**
+     * @constructor
+     * @param options {ISwiperOptions} 选项
+     */
     constructor(options: ISwiperOptions) {
       super();
       this._viewWidth = options.viewWidth;
@@ -57,15 +133,30 @@ namespace swiper {
       }
     }
 
+    /**
+     * @public
+     * @set 设置 effect 效果
+     * @param effect {string}
+     */
     public set effect(effect: string) {
       this._effect = effect;
       this._setSlidesPosition();
     }
 
+    /**
+     * @public
+     * @get 获取 effect
+     * @return {string}
+     */
     public get effect(): string {
       return this._effect;
     }
 
+    /**
+     * @public
+     * @get 获取当前激活序号
+     * @return number
+     */
     public get activeIndex(): number {
       const offset = Math.round(this._offset);
       const slidesCount = this._slides.length;
@@ -76,6 +167,11 @@ namespace swiper {
       return index;
     }
 
+    /**
+     * @public
+     * @set 设置当前激活序号
+     * @param index {number}
+     */
     public set activeIndex(index: number) {
       index = Math.round(index);
       const offset = Math.round(this._offset);
@@ -86,11 +182,17 @@ namespace swiper {
       this.goto(toOffset);
     }
 
+    /**
+     * @private 初始化
+     */
     private _init() {
       this._createBg();
       this._createSlidesContainer();
     }
 
+    /**
+     * @private 创建背景，监听触摸事件
+     */
     private _createBg() {
       this.touchEnabled = true;
       this.addEventListener(egret.TouchEvent.TOUCH_BEGIN, this._onTouchBegin, this);
@@ -99,6 +201,9 @@ namespace swiper {
       this.addEventListener(egret.TouchEvent.TOUCH_RELEASE_OUTSIDE, this._onReleaseOutside, this);
     }
 
+    /**
+     * @private 创建 slides 容器
+     */
     private _createSlidesContainer() {
       this._slidesContainer = new egret.DisplayObjectContainer();
       this._slidesContainer.x = 0;
@@ -106,6 +211,10 @@ namespace swiper {
       this.addChild(this._slidesContainer);
     }
 
+    /**
+     * @public 添加一个 slide
+     * @param slide {Slide}
+     */
     public addSlide(slide: Slide) {
       const slideCopy = slide.clone();
       this._slides.push(slide);
@@ -116,6 +225,10 @@ namespace swiper {
       return this;
     }
 
+    /**
+     * @private 开始触摸时回调
+     * @param e {egret.TouchEvent}
+     */
     private _onTouchBegin(e: egret.TouchEvent) {
       this._startX = e.stageX;
       this._startTime = new Date();
@@ -123,6 +236,10 @@ namespace swiper {
       this._startOffset = this._offset;
     }
 
+    /**
+     * @private 触摸移动时回调
+     * @param e {egret.TouchEvent}
+     */
     private _onTouchMove(e: egret.TouchEvent) {
       if (this._touchStart) {
         this._updateOffset(e.stageX);
@@ -130,19 +247,32 @@ namespace swiper {
       }
     }
 
+    /**
+     * @private 触摸移动时回调
+     * @param e {egret.TouchEvent}
+     */
     private _updateOffset(currentX) {
       let offsetX = currentX - this._startX;
       this._offset = this._startOffset + offsetX / this._slideWidth;
     }
 
+    /**
+     * @private 触摸结束时回调
+     */
     private _onTouchEnd(e: egret.TouchEvent) {
       this._stopDrag(e.stageX);
     }
 
+    /**
+     * @private 触摸结束时回调
+     */
     private _onReleaseOutside(e: egret.TouchEvent) {
       this._stopDrag(e.stageX);
     }
 
+    /**
+     * private 结束拖动
+     */
     private _stopDrag(endX: number) {
       this._updateOffset(endX);
       let endTime = new Date();
@@ -164,6 +294,10 @@ namespace swiper {
       this.goto(nearOffset);
     }
 
+    /**
+     * @public 设置偏移量
+     * @param offset {number}
+     */
     public goto(offset: number) {
       this._tween = new TWEEN.Tween(this)
         .to({_offset: offset}, this._backTime)
@@ -182,6 +316,9 @@ namespace swiper {
         .start();
     }
 
+    /**
+     * @private 根据偏移量和滑动效果设置 slide 的位置
+     */
     private _setSlidesPosition() {
       const slides = this._slidesCopy.concat(this._slides);
       _.forEach(slides, (slide, index) => {
@@ -197,6 +334,11 @@ namespace swiper {
       }
     }
 
+    /**
+     * @private 对 slide 应用 effect 效果
+     * @param slide {Slide}
+     * @param index {number} 序号
+     */
     private _useEffect(slide: Slide, index: number) {
       const slidesCount = this._slides.length;
       const totalCount = slidesCount * 2;
